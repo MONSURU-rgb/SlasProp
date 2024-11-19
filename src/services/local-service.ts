fbf00a0d98ce22521599ec32b94eb416e58ca59b
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-11-19 18:31:53 +0100

    feat: add LocalService for managing cookies in localStorage

---
+++ src/services/local-service.ts
@@ -0,0 +1,21 @@
+class LocalService {
+  setCookie = (keyName: string, value: string) => {
+    try {
+      localStorage.setItem(keyName, value);
+    } catch (err) {
+      if (process.env.NODE_ENV === "development") {
+        console.error(`Error setting cookie: ${keyName}`, err);
+      }
+    }
+  };
+
+  getCookie = (keyName: string) => {
+    try {
+      return localStorage.getItem(keyName);
+    } catch (err) {
+      console.error(`Error retrieving cookie: ${keyName}`, err);
+    }
+  };
+}
+
+export const localService = new LocalService();
